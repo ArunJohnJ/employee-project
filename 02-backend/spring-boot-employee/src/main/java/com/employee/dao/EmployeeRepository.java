460e80bd19ab9b19ad479596d6640cd0719603bf
Arun Johnson J <jjohnson.arun@gmail.com>
2020-04-10 11:04:03 +0530

    Remove duplicated directory

--- 02-backend/spring-boot-employee/src/main/java/com/employee/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.employee.dao;
-
-import com.cognizant.employee.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin("http://localhost:4200")
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
